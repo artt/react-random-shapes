bf72d6ab37b758cea77fd41c902ece3453472579
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-07-03 11:36:08 +0700

    reanme

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { RandomShape } from 'random-shape'
+import { RandomShape } from 'react-random-shape'
 
 const App = () => {
   return <RandomShape width={600} height={300} options={{aEdge: "top"}}/>
