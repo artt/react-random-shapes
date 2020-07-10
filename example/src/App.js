95393b7ece6ec70e94b5258fb1abbfd4a09657f5
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-07-10 15:13:10 +0700

    example

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,6 @@
 import React from 'react'
 
-import { RandomHLine, RandomBlob } from 'react-random-shapes'
-
-const override = [
-  {
-    angle: ["r", 0, Math.PI / 4]
-  },
-  "auto",
-  "auto",
-  {
-    x: ["p", 500],
-    y: ["r", 10, 15] 
-  },
-  "auto"
-]
+import { RandomBlob } from 'react-random-shapes'
 
 const App = () => {
   // return <RandomHLine width={600} height={300} options={{numControls: 4, debug: true}} />
